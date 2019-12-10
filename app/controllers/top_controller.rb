11b3d85e0f822cd9066a593a44bdb94919a42a8e
abe yukari <sakura@AbenoMacBook-Air.local>
2019-12-10 19:16:10 +0900

    コメント機能をつけた

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -6,5 +6,12 @@ class TopController < ApplicationController
   end
 
   def create
+    Top.create(name: top_params[:name], image: top_params[:image], comment: top_params[:comment])
+    redirect_to root_path,notice: 'コメントを作成しました'
+  end
+
+  private
+  def top_params
+    params.permit(:id, :name, :image, :comment)
   end
 end
